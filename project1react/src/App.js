955975ac9e2633ed001b868d2ab5c45898429c32
[KrisPalka1981] <[raptor99@op.pl]>
2023-02-08 23:26:32 +0100

    final version

--- project1react/src/App.js
+++ project1react/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Employees from "./components/Employees";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import ServicesList from "./components/Services";
+import {services} from "./data/data";
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <>
+            <Nav />
+            <Header />
+            <Employees />
+            <ServicesList data={services} />
+            <Footer />
+        </>
+    );
 }
-
 export default App;
